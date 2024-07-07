c5572ab632015f611baf687d6bbcbc610d5813a0
maddaf <md.daf001@gmail.com>
2024-07-07 13:13:21 +0600

    nav_link

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css\style.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
@@ -16,9 +16,9 @@
     <header>
         <nav>
             <div class="nav_logo">
-                <h3><a href="home.php"></a>AutoMob</h3>
+                <h1><a href="home.php"></a>AutoMob</h1>
             </div>
-            <ul class="nav-link">
+            <ul class="nav_link">
                 <li><a href="customer\myorders.php">My Orders</a></li>
                 <li><a href="customer\cart.php"></a>Cart</li>
             </ul>
