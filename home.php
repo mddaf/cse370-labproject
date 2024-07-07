82d12d55910e0121b6f7c6750426ede6266f276d
maddaf <md.daf001@gmail.com>
2024-07-07 12:50:36 +0600

    adding files3

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap"
+      rel="stylesheet"
+    />
+    <title>AutoMob</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="nav_logo">
+                <h3><a href="home.php"></a>AutoMob</h3>
+            </div>
+            <ul class="nav-link">
+                <li><a href="customer\myorders.php">My Orders</a></li>
+                <li><a href="customer\cart.php"></a>Cart</li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap"
+      rel="stylesheet"
+    />
+    <title>AutoMob</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="nav_logo">
+                <h3><a href="index.php">AutoMob</a></h3>
+            </div>
+            <ul class="nav_link">
+                <li><a href="">Log in</a></li>
+                <li><a href="signup.php">Sign up</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
