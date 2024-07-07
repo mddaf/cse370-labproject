ef9940e96625cae92a7b2d25f17c5810c745a91e
maddaf <md.daf001@gmail.com>
2024-07-07 13:35:48 +0600

    anchor fix

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <meta charset="UTF-8" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <link rel="stylesheet" href="css/style.css" >
+    <link rel="preconnect" href="https://fonts.googleapis.com" >
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin >
     <link
       href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap"
-      rel="stylesheet"
-    />
+      rel="stylesheet">
     <title>AutoMob</title>
 </head>
 <body>
     <header>
         <nav>
             <div class="nav_logo">
-                <h3><a href="index.php">AutoMob</a></h3>
+                <h1><a href="index.php">AutoMob</a></h1>
             </div>
             <ul class="nav_link">
-                <li><a href="">Log in</a></li>
+                <li><a href="login.php">Log in</a></li>
                 <li><a href="signup.php">Sign up</a></li>
             </ul>
         </nav>
